c38dffaec25874376c6574821cbb11902c5914cb
Andrew Wick <a.wick17@gmail.com>
2016-12-20 17:15:44 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  #return array.join(" and ")
+  if array.length == 1
+    result = array[0]
+  elsif array.length == 2
+    result = array.join(" and ")
+  else
+    result = "#{array[0, array.length - 1].join(', ')}, and #{array.last}"
+  end
+  return result
 end
+
